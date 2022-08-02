6b749281e9d742e82ef75d39bba93041a57b5ad0
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-08-02 15:26:46 +0000

    fix: convex staking submodule on blocks before locker was deployed

--- eth_portfolio/staking/convex.py
+++ eth_portfolio/staking/convex.py
@@ -4,14 +4,15 @@ from decimal import Decimal
 from typing import Dict, Optional
 
 from eth_portfolio.staking.base import StakingPool
+from eth_portfolio.typing import StakedTokenBalances
 from multicall.utils import await_awaitable
 from y import Contract
 from y.classes.common import ERC20
+from y.contracts import contract_creation_block
 from y.datatypes import Address, Block
 
-from eth_portfolio.typing import StakedTokenBalances
-
 convex_locker_v2 = Contract("0x72a19342e8F1838460eBFCCEf09F6585e32db86E")
+locker_deploy_block = contract_creation_block(convex_locker_v2)
 cvx = ERC20("0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B")
 
 
@@ -26,6 +27,8 @@ class Convex(StakingPool):
         return await_awaitable(coro)
     
     async def balances_async(self, address: Address, block: Optional[Block] = None) -> StakedTokenBalances:
+        if block and block < locker_deploy_block:
+            return {}
         balance = await convex_locker_v2.balanceOf.coroutine(address, block_identifier=block)
         if balance:
             scale, price = await asyncio.gather(cvx.scale, cvx.price_async(block))
