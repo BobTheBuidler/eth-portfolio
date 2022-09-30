1c13a5166fc1a920a5264aa47d9808957d6a08fa
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2022-09-29 23:11:15 -0400

    fix: specify bad web3 versions

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     install_requires=[
         "checksum_dict>=1.1.2",
         "pandas>=1.4.3,<1.5",
+        "web3!=5.29.*,!=5.30.*",
         "ypricemagic>=1.2.2",
     ],
     setup_requires=[
