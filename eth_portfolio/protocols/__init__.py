ab78ee5fe825ad34a1dee8f0204d14ff498d07aa
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2022-09-30 19:29:31 -0400

    fix: TypeError

--- eth_portfolio/protocols/__init__.py
+++ eth_portfolio/protocols/__init__.py
@@ -23,6 +23,6 @@ class ExternalBalances:
     
     async def _balances_async(self, address: Address, block: Optional[Block] = None) -> RemoteTokenBalances:
         balances = await asyncio.gather(*[protocol._balances_async(address, block) for protocol in self.protocols])
-        return {type(protocol).__name__: protocol_balances for protocol, protocol_balances in zip(self.protocols, balances)}
+        return {type(protocol).__name__: protocol_balances for protocol, protocol_balances in zip(self.protocols, balances) if protocol_balances is not None}
 
 _external = ExternalBalances(asynchronous=True)
