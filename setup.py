a7b3259d1a450d28bd73b16be986a9f6fd0e81ed
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-10-06 23:22:57 +0000

    chore: bump ypricemagic version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         "checksum_dict>=1.1.2",
         "pandas>=1.4.3,<1.5",
-        "ypricemagic>=1.4.4",
+        "ypricemagic>=1.4.5",
     ],
     setup_requires=[
         'setuptools_scm',
