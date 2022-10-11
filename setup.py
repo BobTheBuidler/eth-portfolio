b396e3cda4c016598a111d15532d86c7276f04e0
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2022-10-11 18:10:52 -0400

    Add token address to df (#3)
    * chore: bump ypricemagic
    
    * feat: add token_address to dataframe

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         "checksum_dict>=1.1.2",
         "pandas>=1.4.3,<1.5",
-        "ypricemagic>=1.4.7",
+        "ypricemagic>=1.4.8",
     ],
     setup_requires=[
         'setuptools_scm',
