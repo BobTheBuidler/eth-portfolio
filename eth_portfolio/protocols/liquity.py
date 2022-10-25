32de299ad6892534a9c4b549e6b63ff576fce1c0
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-10-25 18:01:48 +0000

    fea: LQTY staking

--- eth_portfolio/protocols/liquity.py
+++ eth_portfolio/protocols/liquity.py
@@ -4,6 +4,12 @@ from y import Network
 from y.classes.common import ERC20
 
 
+class LqtyStakingPool(SingleTokenStakingPoolABC):
+    networks = [Network.Mainnet]
+    contract_address = "0x4f9Fbb3f1E99B56e0Fe2892e623Ed36A76Fc605d"
+    balance_method_name = "stakes"
+    token = ERC20("0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D")  # LQTY
+
 class LiquityStabilityPool(SingleTokenStakingPoolABC):
     networks = [Network.Mainnet]
     contract_address = "0x66017D22b0f8556afDd19FC67041899Eb65a21bb"
