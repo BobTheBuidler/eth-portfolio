98fd9720f2ca3151724f8c3c748f406e6173850e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-09-08 18:28:00 +0000

    feat: Structs

---
+++ eth_portfolio/structs.py
@@ -0,0 +1,47 @@
+from decimal import Decimal
+from typing import Optional
+
+from msgspec import Struct
+from y import Network
+from y.datatypes import Address, Block
+
+
+class _LedgerEntryBase(Struct, kw_only=True):
+    chainid: Network
+    block_number: Block
+    transaction_index: int
+    hash: str
+    from_address: Optional[Address]
+    to_address: Optional[Address]
+    value: Decimal
+    price: Optional[Decimal]
+    value_usd: Optional[Decimal]
+
+class Transaction(_LedgerEntryBase, kw_only=True):
+    block_hash: str
+    nonce: int
+    type: int
+    gas: int
+    gas_price: int
+    input: str
+    r: str
+    s: str
+    v: str
+
+class InternalTransfer(_LedgerEntryBase, kw_only=True):
+    block_hash: str
+    type: str
+    call_type: str
+    trace_address: Address
+    gas: int
+    gas_used: Optional[int]
+    input: str
+    output: str
+    subtraces: int
+    address: Address = None
+
+class TokenTransfer(_LedgerEntryBase, kw_only=True):
+    log_index: int
+    token: Optional[str]
+    token_address: Address
+    value: Decimal
