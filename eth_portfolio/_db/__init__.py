73d1f451356871fac3387ce31ccbb3a788d9214e
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-09-09 17:51:54 +0000

    chore: add err msg wrt extending ypm db

--- eth_portfolio/_db/__init__.py
+++ eth_portfolio/_db/__init__.py
@@ -1,6 +1,12 @@
+from pony.orm import OperationalError
 from y._db.config import connection_settings
 
 from eth_portfolio._db.entities import db
 
 db.bind(**connection_settings, create_db=True)
-db.generate_mapping(create_tables=True)
+try:
+    db.generate_mapping(create_tables=True)
+except OperationalError as e:
+    if str(e).startswith("no such column:"):
+        raise OperationalError("Since eth-portfolio extends the ypricemagic database with additional column definitions, you will need to delete your ypricemagic database at ~/.ypricemagic and rerun this script")
+    raise e
