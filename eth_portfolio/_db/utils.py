5b85fee34e2edf73aef9c6c254c0856fb72fe745
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-06 23:23:50 +0000

    fix: bind db to pg

--- eth_portfolio/_db/utils.py
+++ eth_portfolio/_db/utils.py
@@ -9,7 +9,7 @@ from a_sync import a_sync
 from a_sync.primitives.executor import AsyncProcessPoolExecutor
 from msgspec import json
 from multicall.utils import get_event_loop
-from pony.orm import BindingError, OperationalError, commit, db_session, flush, ProgrammingError
+from pony.orm import BindingError, OperationalError, commit, db_session, flush
 from y import ERC20
 from y.constants import EEE_ADDRESS
 from y.exceptions import NonStandardERC20
@@ -24,11 +24,11 @@ logger = logging.getLogger(__name__)
 
 def __bind(create_db: bool = True):
     try:
-        db.bind(**config.connection_settings, create_db=True)
+        db.bind(**config.connection_settings, create_db=create_db)
     except BindingError as e:
         if not str(e).startswith('Database object was already bound to'):
             raise e
-    except ProgrammingError as e:
+    except Exception as e:
         if 'invalid connection option "create_db"' not in str(e):
             raise e
         __bind(create_db=False)
