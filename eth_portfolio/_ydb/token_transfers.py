acc0a40c7005b9549dcae768427a82b3cababd01
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-11 02:06:09 +0000

    feat: rewrap with ASyncIterable

--- eth_portfolio/_ydb/token_transfers.py
+++ eth_portfolio/_ydb/token_transfers.py
@@ -79,15 +79,6 @@ class TokenTransfers(a_sync.ASyncIterable[TokenTransfer]):
         return self.yield_thru_block(chain.height).__aiter__()
     
     def yield_thru_block(self, block: int) -> a_sync.ASyncIterable["asyncio.Task[TokenTransfer]"]:
-        #return a_sync.ASyncIterable.wrap(self._yield_thru_block(block))
-        return self._yield_thru_block(block)
-    
-    async def _yield_thru_block(self, block: int) -> AsyncIterator["asyncio.Task[TokenTransfer]"]:
-        from a_sync.utils.iterators import _Done # TODO: fix this property and get rid of its
-        async for transfer in a_sync.as_yielded(self.transfers_in.yield_thru_block(block), self.transfers_out.yield_thru_block(block)):
-            while isinstance(transfer, list) and len(transfer) == 1:
-                logger.debug("%s is a list, wtf? unwrapping", transfer)
-                transfer = transfer[0]
-            if isinstance(transfer, _Done):
-                return
-            yield transfer
+        return a_sync.ASyncIterable.wrap(
+            a_sync.as_yielded(self.transfers_in.yield_thru_block(block), self.transfers_out.yield_thru_block(block))
+        )
