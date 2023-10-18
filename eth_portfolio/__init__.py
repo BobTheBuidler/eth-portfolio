f4f7e176b181cb010839805b7a37bec18440d15d
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2023-10-18 03:56:10 +0000

    fix: db initialization in certain cases

--- eth_portfolio/__init__.py
+++ eth_portfolio/__init__.py
@@ -1,4 +1,6 @@
 from eth_portfolio.portfolio import Portfolio, portfolio
+# make sure we init the extended db before we init ypm somewhere
+from eth_portfolio._db import utils
 
 __all__ = [
     "Portfolio",
