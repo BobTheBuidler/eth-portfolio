cf82dcb4e44ba5757cc6a5a960578a95e0d33e54
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-03-04 22:44:00 +0000

    fix: concurrency conflict

--- eth_portfolio/_ledgers/address.py
+++ eth_portfolio/_ledgers/address.py
@@ -83,6 +83,7 @@ class AddressLedgerBase(a_sync.ASyncGenericBase, _AiterMixin[T], Generic[_Ledger
         return self.portfolio_address.portfolio._start_block
 
     async def _get_and_yield(self, start_block: Block, end_block: Block) -> AsyncGenerator[T, None]:
+        yielded = set()
         for obj in self.objects:
             block = obj.block_number
             if block < start_block:
@@ -90,8 +91,21 @@ class AddressLedgerBase(a_sync.ASyncGenericBase, _AiterMixin[T], Generic[_Ledger
             elif end_block and block > end_block:
                 break
             yield obj
+            yielded.add(obj)
         async for obj in self._get_new_objects(start_block, end_block):
-            yield obj
+            if obj not in yielded:
+                yield obj
+                yielded.add(obj)
+        for obj in self.objects:
+            block = obj.block_number
+            if block < start_block:
+                continue
+            elif end_block and block > end_block:
+                break
+            if obj not in yielded:
+                yield obj
+                yielded.add(obj)
+        
     
     @set_end_block_if_none
     @stuck_coro_debugger
