ddc17567dfaa90089b533400862bc323ce3d028d
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-03-24 22:45:19 -0400

    feat: import SHITCOINS to main module

--- eth_portfolio/__init__.py
+++ eth_portfolio/__init__.py
@@ -1,9 +1,11 @@
 from eth_portfolio.portfolio import Portfolio, portfolio
 # make sure we init the extended db before we init ypm somewhere
 from eth_portfolio._db import utils
+from eth_portfolio._shitcoins import SHITCOINS
 
 __all__ = [
     "Portfolio",
-    "portfolio"
+    "portfolio",
+    "SHITCOINS",
 ]
 
