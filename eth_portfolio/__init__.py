8cbc8a919db016ff59dc9952757d98eec582012c
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-05-06 02:56:15 +0000

    feat: use smart task factory

--- eth_portfolio/__init__.py
+++ eth_portfolio/__init__.py
@@ -1,3 +1,9 @@
+
+from a_sync import _smart, _helpers
+
+_smart.set_smart_task_factory(_helpers.get_event_loop())
+
+
 from eth_portfolio.portfolio import Portfolio, portfolio
 # make sure we init the extended db before we init ypm somewhere
 from eth_portfolio._db import utils
