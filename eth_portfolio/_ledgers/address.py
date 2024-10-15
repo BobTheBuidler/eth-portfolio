35919bac651bba7e2d11b81efa4996cf666118c6
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-10-15 08:20:34 -0400

    chore: refactor out _get_status

--- eth_portfolio/_ledgers/address.py
+++ eth_portfolio/_ledgers/address.py
@@ -386,26 +386,7 @@ trace_filter = a_sync.Semaphore(32, __name__ + ".trace_semaphore")(
     eth_retry.auto_retry(dank_mids.eth.trace_filter)
 )
 
-async def _get_status(txhash: str) -> int:
-    """
-    Asynchronously retrieves the transaction status for the given hash.
-
-    Args:
-        txhash: The hexadecimal string representation of the transaction hash.
-
-    Returns:
-        int: The status of the transaction (1 for success, 0 for failure).
-
-    Example:
-        >>> status = await _get_status(txhash="0x123..."); print(status)
-
-    Note:
-        This function is primarily used within :func:`~eth_portfolio._loaders.internal_transfer.load_internal_transfer` to verify
-        transaction success for non-reward transfers. The caching mechanism helps reduce
-        redundant blockchain queries, improving overall performance of the eth_portfolio system.
-    """
-    receipt = await get_transaction_receipt(txhash)
-    return receipt.status
+get_transaction_status = alru_cache(maxsize=None)(dank_mids.eth.get_transaction_status)
 
 @cache_to_disk
 @eth_retry.auto_retry
@@ -422,7 +403,7 @@ async def get_traces(filter_params: TraceFilterParams) -> List[FilterTrace]:
     """
     traces = []
 
-    check_status = a_sync.TaskMapping(_get_status)
+    check_status = a_sync.TaskMapping(get_transaction_status)
   
     for trace in await trace_filter(filter_params) :
         if trace.error:
