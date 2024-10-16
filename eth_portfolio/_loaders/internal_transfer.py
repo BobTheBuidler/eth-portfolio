683fbd202dee7ee5a23ef73463d9a9596f82bdaa
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-10-15 21:11:17 -0400

    chore: refactor

--- eth_portfolio/_loaders/internal_transfer.py
+++ eth_portfolio/_loaders/internal_transfer.py
@@ -72,12 +72,9 @@ async def load_internal_transfer(trace: FilterTrace, load_prices: bool) -> Inter
     params = {"trace": trace}
 
     if load_prices:
-        block = trace.block
-        value = trace.action.value
-        del trace  # we dont need to maintain this reference while we fetch the price
-        price = round(Decimal(await _get_price(EEE_ADDRESS, block)), 18)
+        price = round(Decimal(await _get_price(EEE_ADDRESS, trace.block)), 18)
         params['price'] = price
-        params['value_usd'] = round(value * price, 18)
+        params['value_usd'] = round(trace.action.value * price, 18)
     
     return InternalTransfer(**params)
     
