0ba78f2a2ab89c4cdcad03c1665fe62a96b5f78a
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-10-16 18:05:43 +0000

    Update internal_transfer.py

--- eth_portfolio/_loaders/internal_transfer.py
+++ eth_portfolio/_loaders/internal_transfer.py
@@ -3,7 +3,7 @@ Loader functions for processing internal transfers on the blockchain.
 
 This module provides utilities for loading and processing internal transfers, 
 including those related to rewards like block and uncle rewards. It also 
-handles price loading for the transferred value and checksum addresses.
+handles price loading for the transferred value.
 
 The module implements caching mechanisms and error handling to optimize 
 performance and ensure robustness when dealing with blockchain data.
@@ -12,7 +12,6 @@ Key components:
 - Transaction status retrieval
 - Internal transfer processing and conversion
 - Reward type identification (block and uncle rewards)
-- Address checksumming and value conversion
 - Price loading for transferred value (optional)
 """
 
@@ -20,14 +19,14 @@ from decimal import Decimal
 from typing import Optional
 
 from brownie import chain
+from dank_mids.structs import Trace
 from y._decorators import stuck_coro_debugger
 from y.constants import EEE_ADDRESS
 
 from eth_portfolio._cache import cache_to_disk
-from eth_portfolio._loaders.utils import checksum, get_transaction_receipt, underscore
+from eth_portfolio._loaders.utils import get_transaction_receipt
 from eth_portfolio._utils import _get_price
 from eth_portfolio.structs import InternalTransfer
-
 
 @cache_to_disk
 async def _get_status(txhash: str) -> int:
@@ -65,7 +64,6 @@ async def load_internal_transfer(transfer: dict, load_prices: bool) -> Optional[
     transaction success for non-reward transfers.
 
     The function performs several data transformations:
-    - Address checksumming
     - Value and gas conversions
     - Optional USD price loading
     - Field standardization
@@ -84,162 +82,71 @@ async def load_internal_transfer(transfer: dict, load_prices: bool) -> Optional[
         >>> internal_tx = await load_internal_transfer(transfer=transfer, load_prices=True); print(internal_tx)
 
     Note:
-        - This function relies on several helper functions (:func:`~eth_portfolio._loaders.internal_transfer.is_block_reward`, :func:`~eth_portfolio._loaders.internal_transfer.is_uncle_reward`,
-          :func:`~eth_portfolio._loaders.internal_transfer._get_status`) and utility functions from other modules (:func:`~eth_portfolio._loaders.utils.checksum`, :func:`~eth_portfolio._utils._get_price`,
-          :func:`~eth_portfolio._loaders.utils.underscore`).
         - Transfers to the Gnosis Safe Singleton (0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552) are filtered out
           as they typically don't represent actual value transfers.
         - The `traceAddress` is converted to a string for consistent representation across different scenarios.
         - For block and uncle rewards, `gas` is set to 0 as these are not regular transactions.
         - When loading prices, the `EEE_ADDRESS` constant is used, which represents the native currency of the chain.
-        - The `underscore` function is used to convert camelCase keys to snake_case for the InternalTransfer struct.
 
     Integration with eth_portfolio ecosystem:
         - Uses the InternalTransfer struct from eth_portfolio.structs for standardized output.
         - Utilizes utility functions from eth_portfolio._loaders.utils and eth_portfolio._utils.
         - Interacts with the global 'chain' object from the brownie library for chain ID.
     """
-    if transfer.get("to") == "0xd9db270c1b5e3bd161e8c8503c55ceabee709552":
-        # "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552":  # Gnosis Safe Singleton 1.3.0
+    if trace.to == "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552":  # Gnosis Safe Singleton 1.3.0
         # NOTE: Not sure why these appear, but I've yet to come across an internal transfer
         # that actually transmitted value to the singleton even though they appear to.
         return None
-        
-    if is_block_reward(transfer):
-        transfer['transactionHash'] = 'block reward'
-    elif is_uncle_reward(transfer):
-        transfer['transactionHash'] = 'uncle reward'
+
+    params = {}
+
+    if trace.type == "reward:
+        if trace.action.rewardType == 'block':
+            params = {'hash': 'block reward'}
+        elif trace.action.rewardType == 'uncle':
+            params = {'hash': 'uncle reward'}
+        else:
+            raise NotImplementedError(trace.action.rewardType)
     else:
         # NOTE: We don't need to confirm block rewards came from a successful transaction, because they don't come from a transaction
         # In all other cases, we need to confirm the transaction didn't revert
-        if await _get_status(transfer['transactionHash']) == 0:
+        if await _get_status(trace.transactionHash) == 0:
             return None
+            
+        params = {'hash': trace.transactionHash}
+
+    
+    params['transaction_index'] = trace.transactionPosition
+    params['chainid'] = chain.id
 
     # Un-nest the action dict
-    if action := transfer.pop('action', None):
+    if action := trace.action:
         for key, value in action.items():
             if key == 'author':
                 # for block reward transfers, the recipient is 'author'
-                transfer['to'] = value
+                params['to'] = value
             else:
-                transfer[key] = value
+                params[key] = value
             
     # Un-nest the result dict
-    if result := transfer.pop('result', None):
+    if result := trace.result:
         for key, value in result.items():
-            transfer[key] = value
+            params[key] = value
 
-    # Checksum the addresses
-    if "from" in transfer:
-        transfer['from_address'] = checksum(transfer.pop('from'))
-    if "to" in transfer:
-        transfer['to_address'] = checksum(transfer.pop('to'))
-    if "address" in transfer:
-        transfer['address'] = checksum(transfer.pop('address'))
-        
-    transfer['traceAddress'] = str(transfer['traceAddress'])
-    transfer['value'] = Decimal(int(transfer['value'], 16)) / Decimal(10**18)
-    transfer['gas'] = 0 if is_block_reward(transfer) or is_uncle_reward(transfer) else int(transfer['gas'], 16)
-    transfer['gasUsed'] = int(transfer['gasUsed'], 16) if transfer.get('gasUsed') else None
+    # Remap the addresses
+    if trace.sender:
+        params['from_address'] = trace.sender
+    if trace.to:
+        params['to_address'] = trace.to
+
+    value = trace.value_scaled
+    params['value'] = value
+    params['gas'] = 0 if trace.type == "reward" and trace.action.rewardType in ["block", "uncle"] else trace.gas
 
     if load_prices:
-        price = round(Decimal(await _get_price(EEE_ADDRESS, transfer['blockNumber'])), 18)
-        transfer['price'] = price
-        transfer['value_usd'] = round(transfer['value'] * price, 18)
-        
-    transfer['hash'] = transfer.pop('transactionHash')
-    transfer['transaction_index'] = transfer.pop('transactionPosition', None)
-    transfer['chainid'] = chain.id
-
-    # We include this data in the hash field, we don't need it anymore
-    transfer.pop('rewardType', None)
+        price = round(Decimal(await _get_price(EEE_ADDRESS, trace.blockNumber)), 18)
+        params['price'] = price
+        params['value_usd'] = round(value * price, 18)
     
-    return InternalTransfer(**{underscore(k): v for k, v in transfer.items()})
-
-def is_block_reward(transfer: dict) -> bool:
-    """
-    Determines if the given transfer represents a block reward.
-
-    This function works in conjunction with :func:`~eth_portfolio._loaders.internal_transfer.get_reward_type` to identify
-    block reward transfers, which are treated differently in the main processing function.
-
-    Args:
-        transfer: The transfer dictionary to check. Expected to have a 'type' key and either a 'rewardType' key
-                  or a nested 'action' dictionary with a 'rewardType' key.
-
-    Returns:
-        bool: True if the transfer is a block reward, False otherwise.
-
-    Example:
-        >>> transfer = {"type": "reward", "rewardType": "block"}
-        >>> print(is_block_reward(transfer=transfer))  # Output: True
-
-    Note:
-        This function is used within :func:`~eth_portfolio._loaders.internal_transfer.load_internal_transfer` to handle
-        special cases for block rewards. Block rewards are not associated with
-        regular transactions and require different processing in the eth_portfolio system.
-    """
-    return transfer['type'] == 'reward' and get_reward_type(transfer) == 'block'
-
-def is_uncle_reward(transfer: dict) -> bool:
-    """
-    Determines if the given transfer represents an uncle reward.
-
-    Similar to :func:`~eth_portfolio._loaders.internal_transfer.is_block_reward`, this function works with :func:`~eth_portfolio._loaders.internal_transfer.get_reward_type`
-    to identify uncle reward transfers, which are also treated as special
-    cases in the main processing function.
-
-    Args:
-        transfer: The transfer dictionary to check. Expected to have a 'type' key and either a 'rewardType' key
-                  or a nested 'action' dictionary with a 'rewardType' key.
-
-    Returns:
-        bool: True if the transfer is an uncle reward, False otherwise.
-
-    Example:
-        >>> transfer = {"type": "reward", "rewardType": "uncle"}
-        >>> print(is_uncle_reward(transfer=transfer))  # Output: True
-
-    Note:
-        This function is used within :func:`~eth_portfolio._loaders.internal_transfer.load_internal_transfer` to handle
-        special cases for uncle rewards. Uncle rewards, like block rewards,
-        are not associated with regular transactions and require different
-        processing in the eth_portfolio system.
-    """
-    return transfer['type'] == 'reward' and get_reward_type(transfer) == 'uncle'
-
-def get_reward_type(transfer: dict) -> str:
-    """
-    Retrieves the reward type from a transfer dictionary.
-
-    This function handles the potential nested structure of the transfer
-    dictionary, looking for the 'rewardType' key in either the main dict
-    or a nested 'action' dict.
-
-    Args:
-        transfer: The transfer dictionary containing the reward type information. Expected to have either
-                  a 'rewardType' key or a nested 'action' dictionary with a 'rewardType' key.
-
-    Returns:
-        str: The reward type, either 'block' or 'uncle'.
-
-    Raises:
-        ValueError: If the transfer is not of reward type (i.e., neither location contains a 'rewardType' key).
-
-    Example:
-        >>> transfer = {"type": "reward", "rewardType": "block"}; print(get_reward_type(transfer=transfer))  # Output: 'block'
-        >>> transfer = {"type": "reward", "action": {"rewardType": "uncle"}}; print(get_reward_type(transfer=transfer))  # Output: 'uncle'
-        >>> transfer = {"type": "call", "value": "0x0"}; get_reward_type(transfer=transfer)  # Raises ValueError
-
-    Note:
-        This function is used by both :func:`~eth_portfolio._loaders.internal_transfer.is_block_reward` and :func:`~eth_portfolio._loaders.internal_transfer.is_uncle_reward`
-        to determine the specific type of reward transfer. It's crucial for
-        correctly processing different types of rewards in the eth_portfolio system.
-        The function accounts for potential variations in the structure of the
-        transfer dictionary, which may occur due to differences in data sources
-        or blockchain explorers.
-    """
-    try:
-        return transfer.get('rewardType') or transfer['action']['rewardType']
-    except KeyError:
-        raise ValueError('transfer is not reward type') from None
+    return InternalTransfer(**params)
+    
