5807d859ad2ed484aad4c5f93274dcdfdd4b20f3
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-20 06:33:16 +0000

    feat: more debuggers...

--- eth_portfolio/_db/utils.py
+++ eth_portfolio/_db/utils.py
@@ -268,10 +268,10 @@ def _insert_transaction(transaction: Transaction) -> None:
             raise ValueError(UNSET, x)
     if "UNSET" in repr(transaction):
         raise ValueError(transaction)
-    debugger = {k: getattr(transaction, k) for k in dir(transaction) if hasattr(transaction, k)}
+    debugger = {k: getattr(transaction, k) for k in dir(transaction) if hasattr(transaction, k) and "method" not in type(getattr(transaction, k)).__name__}
     if "UNSET" in repr(debugger):
         raise ValueError(debugger)
-    debugger = {k: getattr(transaction.transaction, k) for k in dir(transaction.transaction) if hasattr(transaction, k)}
+    debugger = {k: getattr(transaction.transaction, k) for k in dir(transaction.transaction) if hasattr(transaction.transaction, k) and "method" not in type(getattr(transaction.transaction, k)).__name__}
     if "UNSET" in repr(debugger):
         raise ValueError(debugger)
 
