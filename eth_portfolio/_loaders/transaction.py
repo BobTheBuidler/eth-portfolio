7a82da79a6ca797645aa945474dbf8dec5bdf655
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-10-22 14:31:02 +0000

    fix: str has no attr get_receipt

--- eth_portfolio/_loaders/transaction.py
+++ eth_portfolio/_loaders/transaction.py
@@ -171,13 +171,13 @@ async def get_transaction_by_nonce_and_block(address: Address, nonce: int, block
         receipt_bytes: msgspec.Raw
         # Special handler for contract creation transactions
         if tx.to is None:
-            receipt_bytes = await get_transaction_receipt(tx.hash)
+            receipt_bytes = await get_transaction_receipt(tx.hash.hex())
             receipt_0 = msgspec.json.decode(receipt_bytes, type=ReceiptContractAddress, dec_hook=dank_structs.data.Address._decode_hook)
             if receipt_0.contractAddress == address:
                 return tx
         # Special handler for Gnosis Safe deployments
         elif tx.to == "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2":
-            receipt_bytes = await get_transaction_receipt(tx.hash)
+            receipt_bytes = await get_transaction_receipt(tx.hash.hex())
             receipt_1 = msgspec.json.decode(receipt_bytes, type=ReceiptLogs, dec_hook=dank_structs.data._decode_hook)
             events = decode_logs(receipt_1.logs)
             if "SafeSetup" in events and "ProxyCreation" in events and any(event['proxy'] == address for event in events['ProxyCreation']):
