a4fa9dc9f808a3ddc098983f253fc0be84543484
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-18 21:08:10 -0400

    fix: check block for maker

--- eth_portfolio/protocols/lending/maker.py
+++ eth_portfolio/protocols/lending/maker.py
@@ -56,6 +56,9 @@ class Maker(LendingProtocolWithLockedCollateral):
 
     @stuck_coro_debugger
     async def _debt(self, address: Address, block: Optional[int] = None) -> TokenBalances:
+        if block is not None and block <= await contract_creation_block_async(self.ilk_registry):
+            return TokenBalances(block=block)
+            
         ilks, urn = await asyncio.gather(self.get_ilks(block), self._urn(address))
 
         data = await asyncio.gather(
