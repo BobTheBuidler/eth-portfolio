e74e9a0855cd678e04dfb8b0175a9908421ac451
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-12-16 09:21:16 -0400

    Update token_transfers.py

--- eth_portfolio/_ydb/token_transfers.py
+++ eth_portfolio/_ydb/token_transfers.py
@@ -1,6 +1,6 @@
-import abc
-import asyncio
-import logging
+from abc import abstractproperty
+from asyncio import Task, create_task, sleep
+from logging import getLogger
 from typing import AsyncIterator, List
 
 import a_sync
@@ -27,10 +27,12 @@ except ImportError:
 
     encode_address = lambda address: encode_hex(encode_single("address", str(address)))
 
-logger = logging.getLogger(__name__)
+logger = getLogger(__name__)
+_logger_is_enabled_for = logger.isEnabledFor
+_logger_log = logger._log
 
 
-class _TokenTransfers(ProcessedEvents["asyncio.Task[TokenTransfer]"]):
+class _TokenTransfers(ProcessedEvents["Task[TokenTransfer]"]):
     """A helper mixin that contains all logic for fetching token transfers for a particular wallet address"""
 
     __slots__ = "address", "_load_prices"
@@ -43,44 +45,51 @@ class _TokenTransfers(ProcessedEvents["asyncio.Task[TokenTransfer]"]):
     def __repr__(self) -> str:
         return f"<{self.__class__.__module__}.{self.__class__.__name__} address={self.address}>"
 
-    @abc.abstractproperty
+    @abstractproperty
     def _topics(self) -> List: ...
 
     @a_sync.ASyncIterator.wrap
-    async def yield_thru_block(self, block) -> AsyncIterator["asyncio.Task[TokenTransfer]"]:
-        logger.debug("%s yielding all objects thru block %s", self, block)
+    async def yield_thru_block(self, block) -> AsyncIterator["Task[TokenTransfer]"]:
+        if not _logger_is_enabled_for(DEBUG):
+            async for task in self._objects_thru(block=block):
+                yield task
+                
+        _logger_log(DEBUG, "%s yielding all objects thru block %s", (self, block))
         async for task in self._objects_thru(block=block):
-            logger.debug(
+            _logger_log(
+                DEBUG,
                 "yielding %s at block %s [thru: %s, lock: %s]",
-                task,
-                task.block,
-                block,
-                self._lock.value,
+                (task, task.block, block, self._lock.value),
             )
             yield task
-        logger.debug("%s yield thru %s complete", self, block)
+        _logger_log(DEBUG, "%s yield thru %s complete", (self, block))
 
     async def _extend(self, objs: List[evmspec.Log]) -> None:
         shitcoins = SHITCOINS.get(chain.id, set())
+        done = 0
         for log in objs:
             if log.address in shitcoins:
                 continue
             # save i/o
             array_encodable_log = y._db.log.Log(**log)
-            task = asyncio.create_task(
+            task = create_task(
                 coro=_loaders.load_token_transfer(array_encodable_log, self._load_prices),
                 name="load_token_transfer",
             )
             task.block = log.block  # type: ignore [attr-defined]
             self._objects.append(task)
+            done += 1
+            # Make sure the event loop doesn't get blocked
+            if done % 100 == 0:
+                await sleep(0)
 
-    def _get_block_for_obj(self, task: "asyncio.Task[TokenTransfer]") -> int:
+    def _get_block_for_obj(self, task: "Task[TokenTransfer]") -> int:
         return task.block  # type: ignore [attr-defined]
 
-    def _process_event(self, task: "asyncio.Task[TokenTransfer]") -> "asyncio.Task[TokenTransfer]":
+    def _process_event(self, task: "Task[TokenTransfer]") -> "Task[TokenTransfer]":
         return task
 
-    def _done_callback(self, task: asyncio.Task) -> None:
+    def _done_callback(self, task: Task) -> None:
         if e := task.exception():
             self._exc = e
             logger.exception(e)
@@ -116,7 +125,7 @@ class TokenTransfers(a_sync.ASyncIterable[TokenTransfer]):
     def __aiter__(self):
         return self.yield_thru_block(chain.height).__aiter__()
 
-    def yield_thru_block(self, block: int) -> a_sync.ASyncIterator["asyncio.Task[TokenTransfer]"]:
+    def yield_thru_block(self, block: int) -> a_sync.ASyncIterator["Task[TokenTransfer]"]:
         return a_sync.ASyncIterator(
             a_sync.as_yielded(
                 self.transfers_in.yield_thru_block(block),
