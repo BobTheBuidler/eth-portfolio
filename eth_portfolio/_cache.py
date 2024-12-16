a5859cf4b36b5f54b7d814ff8264c021bb4132ac
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-12-16 02:09:18 -0400

    feat: optimize cache wrap

--- eth_portfolio/_cache.py
+++ eth_portfolio/_cache.py
@@ -1,19 +1,22 @@
-import contextlib
 import functools
-import hashlib
 import inspect
 import os
-import pickle
+from hashlib import md5
+from os import makedirs, path
+from os.path import exists as _path_exists
+from os.path import join as _path_join
+from pickle import dumps, load, loads
 from typing import Any
 
-import a_sync
-import aiofiles
-import aiofiles.os
+from a_sync import PruningThreadPoolExecutor
 from a_sync._typing import AnyFn, P, T
+from a_sync.asyncio import create_task
+from aiofiles import open as _aio_open
+from aiofiles.ospath import exists as _aio_path_exists
 from brownie import chain
 
 BASE_PATH = f"./cache/{chain.id}/"
-EXECUTOR = a_sync.PruningThreadPoolExecutor(32)
+EXECUTOR = PruningThreadPoolExecutor(32)
 
 
 def cache_to_disk(fn: AnyFn[P, T]) -> AnyFn[P, T]:
@@ -22,20 +25,22 @@ def cache_to_disk(fn: AnyFn[P, T]) -> AnyFn[P, T]:
 
     def get_cache_file_path(args, kwargs):
         # Create a unique filename based on the function arguments
-        key = hashlib.md5(pickle.dumps((args, sorted(kwargs.items())))).hexdigest()
-        return os.path.join(cache_path_for_fn, f"{key}.json")
+        key = md5(dumps((args, sorted(kwargs.items())))).hexdigest()
+        return _path_join(cache_path_for_fn, f"{key}.json")
 
-    os.makedirs(cache_path_for_fn, exist_ok=True)
+    makedirs(cache_path_for_fn, exist_ok=True)
 
     if inspect.iscoroutinefunction(fn):
 
         @functools.wraps(fn)
         async def disk_cache_wrap(*args: P.args, **kwargs: P.kwargs) -> T:
             cache_path = get_cache_file_path(args, kwargs)
-            if await aiofiles.os.path.exists(cache_path, executor=EXECUTOR):
-                async with aiofiles.open(cache_path, "rb", executor=EXECUTOR) as f:
-                    with contextlib.suppress(EOFError):
-                        return pickle.loads(await f.read())
+            if await _aio_path_exists(cache_path, executor=EXECUTOR):
+                async with _aio_open(cache_path, "rb", executor=EXECUTOR) as f:
+                    try:
+                        return loads(await f.read())
+                    except EOFError:
+                        pass
 
             async_result: T = await fn(*args, **kwargs)
             await __cache_write(cache_path, async_result)
@@ -46,14 +51,16 @@ def cache_to_disk(fn: AnyFn[P, T]) -> AnyFn[P, T]:
         @functools.wraps(fn)
         def disk_cache_wrap(*args: P.args, **kwargs: P.kwargs) -> T:
             cache_path = get_cache_file_path(args, kwargs)
-            if os.path.exists(cache_path):
+            if _path_exists(cache_path):
                 with open(cache_path, "rb") as f:
-                    with contextlib.suppress(EOFError):
-                        return pickle.load(f)
+                    try:
+                        return load(f)
+                    except EOFError:
+                        pass
 
             sync_result: T = fn(*args, **kwargs)  # type: ignore [assignment, return-value]
             try:
-                a_sync.create_task(
+                create_task(
                     coro=__cache_write(cache_path, result),
                     skip_gc_until_done=True,
                 )
@@ -62,8 +69,9 @@ def cache_to_disk(fn: AnyFn[P, T]) -> AnyFn[P, T]:
             return sync_result
 
     return disk_cache_wrap
-
+    
 
 async def __cache_write(cache_path: str, result: Any) -> None:
-    async with aiofiles.open(cache_path, "wb", executor=EXECUTOR) as f:
-        await f.write(pickle.dumps(result))
+    result = dumps(result)
+    async with _aio_open(cache_path, "wb", executor=EXECUTOR) as f:
+        await f.write(result)
