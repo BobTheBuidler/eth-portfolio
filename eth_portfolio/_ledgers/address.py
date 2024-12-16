1b8fd4643f671ae88a6e8d323d9e753233a43965
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-12-16 22:29:17 +0000

    fix: missing import

--- eth_portfolio/_ledgers/address.py
+++ eth_portfolio/_ledgers/address.py
@@ -50,7 +50,7 @@ from y.utils.events import BATCH_SIZE
 from eth_portfolio import _exceptions, _loaders
 from eth_portfolio._cache import cache_to_disk
 from eth_portfolio._decorators import set_end_block_if_none
-from eth_portfolio._loaders.transaction import get_nonce_at_block
+from eth_portfolio._loaders.transaction import get_nonce_at_block, load_transaction
 from eth_portfolio._utils import PandableList, _AiterMixin, get_buffered_chain_height
 from eth_portfolio._ydb.token_transfers import TokenTransfers
 from eth_portfolio.structs import InternalTransfer, TokenTransfer, Transaction
