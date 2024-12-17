3457f228742cb7fbf59fc967548d5e768bcdcd49
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2024-12-17 16:33:19 +0000

    chore: refactor

--- eth_portfolio/_ydb/token_transfers.py
+++ eth_portfolio/_ydb/token_transfers.py
@@ -77,7 +77,7 @@ class _TokenTransfers(ProcessedEvents["Task[TokenTransfer]"]):
             # save i/o
             array_encodable_log = y._db.log.Log(**log)
             task = create_task(
-                coro=load_token_transfer(array_encodable_log, self._load_prices),
+                load_token_transfer(array_encodable_log, self._load_prices)
             )
             task.block = log.block  # type: ignore [attr-defined]
             append_loader_task(task)
