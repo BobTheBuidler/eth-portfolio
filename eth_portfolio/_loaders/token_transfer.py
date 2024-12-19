d34bb6e7e6df5e5ea0bc3328686906780bcd365f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-12-19 19:58:06 -0400

    chore: cleanup bugfix in last commit

--- eth_portfolio/_loaders/token_transfer.py
+++ eth_portfolio/_loaders/token_transfer.py
@@ -102,9 +102,6 @@ async def load_token_transfer(
                 )
             else:
                 coro_results["transaction_index"] = await tx_index_coro
-
-            if coro_results["transaction_index"] is None:
-                raise TypeError(None, coro_results, transfer_log)
                 
         except Exception as e:
             logger.error(
@@ -201,12 +198,9 @@ async def get_transaction_index(hash: str) -> int:
             break
         logger.info("get_transaction_index failed, retrying...")
 
-    i = msgspec.json.decode(
+    return msgspec.json.decode(
         receipt_bytes, type=HasTxIndex, dec_hook=TransactionIndex._decode_hook
     ).transactionIndex
-    if i is None:
-        raise TypeError(i, msgspec.json.decode(receipt_bytes))
-    return i
 
 
 class HasTxIndex(msgspec.Struct):
