8829e9f5172042eb23c96c5a5e5f3a8eb44b1d33
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-12-21 00:17:30 -0400

    Update address.py

--- eth_portfolio/_ledgers/address.py
+++ eth_portfolio/_ledgers/address.py
@@ -610,7 +610,7 @@ async def get_traces(filter_params: TraceFilterParams) -> List[FilterTrace]:
         logger.warning("polygon doesnt support trace_filter method, must develop alternate solution")
         return []
     semaphore_key = tuple(
-        sorted((filter_params.get(x, [""]) for x in ("toAddress", "fromAddress")))
+        sorted(tuple(filter_params.get(x, ("",)) for x in ("toAddress", "fromAddress")))
     )
     async with _trace_semaphores[semaphore_key]:
         return await _check_traces(await trace_filter(**filter_params))
