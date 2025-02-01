f7ce0095781242158a52b75f34d1e510ceb9e906
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-02-01 14:05:49 -0400

    chore: add debug logging for None nonce

--- eth_portfolio/_loaders/transaction.py
+++ eth_portfolio/_loaders/transaction.py
@@ -302,6 +302,8 @@ async def get_nonce_at_block(address: Address, block: Block) -> int:
         # TODO move this into dank
         while nonce is None:
             nonce = await dank_mids.eth.get_transaction_count(address, block_identifier=block) - 1
+            if nonce is None:
+                logger.info("Rec'd nonce %s for %s at block %s, retrying...", nonce, address, block")
         _update_nonces(address, nonce, block)
         return nonce
     except ValueError as e:
