7c460dfea7dfebd7114ddaef721df36dd1e74b89
nvyncke <nvyncke@DESKTOP-FJSK0BM>
2025-02-03 20:39:56 -0500

    chore: auto-update documentation eth_portfolio/protocols/lending/compound.py

--- eth_portfolio/protocols/lending/compound.py
+++ eth_portfolio/protocols/lending/compound.py
@@ -30,6 +30,26 @@ class Compound(LendingProtocol):
     @alru_cache(ttl=300)
     @stuck_coro_debugger
     async def underlyings(self) -> List[ERC20]:
+        """Fetches the underlying ERC20 tokens for all Compound markets.
+
+        This method gathers all markets from the Compound protocol's trollers
+        and filters out those that do not have a `borrowBalanceStored` attribute.
+        It then separates markets into those that use the gas token and those
+        that have an underlying ERC20 token, fetching the underlying tokens
+        accordingly.
+
+        Returns:
+            A list of :class:`~y.classes.common.ERC20` instances representing the underlying tokens.
+
+        Examples:
+            >>> compound = Compound()
+            >>> underlyings = await compound.underlyings()
+            >>> for token in underlyings:
+            ...     print(token.symbol)
+
+        See Also:
+            - :meth:`markets`: To get the list of market contracts.
+        """
         all_markets: List[List[CToken]] = await gather(
             *[comp.markets for comp in compound.trollers.values()]
         )
@@ -58,10 +78,48 @@ class Compound(LendingProtocol):
     @a_sync.future
     @stuck_coro_debugger
     async def markets(self) -> List[Contract]:
+        """Fetches the list of market contracts for the Compound protocol.
+
+        This method ensures that the underlying tokens are fetched first,
+        as they are used to determine the markets.
+
+        Returns:
+            A list of :class:`~brownie.network.contract.Contract` instances representing the markets.
+
+        Examples:
+            >>> compound = Compound()
+            >>> markets = await compound.markets()
+            >>> for market in markets:
+            ...     print(market.address)
+
+        See Also:
+            - :meth:`underlyings`: To get the list of underlying tokens.
+        """
         await self.underlyings()
         return self._markets
 
     async def _debt(self, address: Address, block: Optional[Block] = None) -> TokenBalances:
+        """Calculates the debt balance for a given address in the Compound protocol.
+
+        This method fetches the borrow balance for each market and calculates
+        the debt in terms of the underlying token and its USD value.
+
+        Args:
+            address: The Ethereum address to calculate the debt for.
+            block: The block number to query. Defaults to the latest block.
+
+        Returns:
+            A :class:`~eth_portfolio.typing.TokenBalances` object representing the debt balances.
+
+        Examples:
+            >>> compound = Compound()
+            >>> debt_balances = await compound._debt("0x1234567890abcdef1234567890abcdef12345678")
+            >>> for token, balance in debt_balances.items():
+            ...     print(f"Token: {token}, Balance: {balance.balance}, USD Value: {balance.usd_value}")
+
+        See Also:
+            - :meth:`debt`: Public method to get the debt balances.
+        """
         # if ypricemagic doesn't support any Compound forks on current chain
         if len(compound.trollers) == 0:
             return TokenBalances(block=block)
@@ -93,6 +151,27 @@ class Compound(LendingProtocol):
 async def _borrow_balance_stored(
     market: Contract, address: Address, block: Optional[Block] = None
 ) -> Optional[int]:
+    """Fetches the stored borrow balance for a given market and address.
+
+    This function attempts to call the `borrowBalanceStored` method on the
+    market contract. If the call reverts, it returns None.
+
+    Args:
+        market: The market contract to query.
+        address: The Ethereum address to fetch the borrow balance for.
+        block: The block number to query. Defaults to the latest block.
+
+    Returns:
+        The stored borrow balance as an integer, or None if the call reverts.
+
+    Examples:
+        >>> market = Contract.from_explorer("0x1234567890abcdef1234567890abcdef12345678")
+        >>> balance = await _borrow_balance_stored(market, "0xabcdefabcdefabcdefabcdefabcdefabcdef")
+        >>> print(balance)
+
+    See Also:
+        - :meth:`_debt`: Uses this function to calculate debt balances.
+    """
     try:
         return await market.borrowBalanceStored.coroutine(str(address), block_identifier=block)
     except ValueError as e:
