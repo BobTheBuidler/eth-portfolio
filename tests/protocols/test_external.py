3ed083d18c0ab160152e36841098a232c93b11eb
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-02-05 17:13:42 -0500

    feat(docs): new docs (#175)

--- tests/protocols/test_external.py
+++ tests/protocols/test_external.py
@@ -10,15 +10,30 @@ SOME_TOKEN = "0x0000000000000000000000000000000000000002"
 SOME_OTHER_TOKEN = "0x0000000000000000000000000000000000000003"
 
 
-class MockProtocolA(AsyncMock): ...
+class MockProtocolA(AsyncMock):
+    """Mock class for simulating Protocol A behavior in tests."""
 
 
-class MockProtocolB(AsyncMock): ...
+class MockProtocolB(AsyncMock):
+    """Mock class for simulating Protocol B behavior in tests."""
 
 
 @patch("a_sync.map")
 @pytest.mark.asyncio
 async def test_balances_no_protocols(mock_map):
+    """Test the `balances` function with no protocols.
+
+    This test verifies that when there are no protocols in the
+    `protocols.protocols` list, the `balances` function returns an
+    empty :class:`~eth_portfolio.typing.RemoteTokenBalances` object
+    and does not call the `a_sync.map` function.
+
+    Args:
+        mock_map: The mock object for `a_sync.map`.
+
+    See Also:
+        - :func:`~eth_portfolio.protocols.balances`
+    """
     protocols.protocols = []
 
     balances = await protocols.balances(SOME_ADDRESS)
@@ -29,6 +44,19 @@ async def test_balances_no_protocols(mock_map):
 
 @pytest.mark.asyncio
 async def test_balances_with_protocols():
+    """Test the `balances` function with multiple protocols.
+
+    This test verifies that when there are multiple protocols in the
+    `protocols.protocols` list, the `balances` function correctly
+    aggregates balances from each protocol.
+
+    The test uses :class:`~unittest.mock.AsyncMock` to simulate
+    protocol behavior and checks that each protocol's `balances`
+    method is called with the correct arguments.
+
+    See Also:
+        - :func:`~eth_portfolio.protocols.balances`
+    """
     mock_protocol_a = MockProtocolA()
     mock_protocol_a.balances.return_value = TokenBalances({SOME_TOKEN: Balance(100, 200)})
 
@@ -52,6 +80,21 @@ async def test_balances_with_protocols():
 
 @pytest.mark.asyncio
 async def test_balances_with_protocols_and_block():
+    """Test the `balances` function with protocols and a specific block.
+
+    This test verifies that when there are multiple protocols in the
+    `protocols.protocols` list and a specific block is provided, the
+    `balances` function correctly aggregates balances from each
+    protocol for the specified block.
+
+    The test uses :class:`~unittest.mock.AsyncMock` to simulate
+    protocol behavior and checks that each protocol's `balances`
+    method is called with the correct arguments, including the block
+    number.
+
+    See Also:
+        - :func:`~eth_portfolio.protocols.balances`
+    """
     block = 1234567
 
     mock_protocol_a = MockProtocolA()
