338ef9d8ec48c1948da47387c5cba16ed0e040dc
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-02-05 22:12:55 +0000

    feat(docs): new docs

--- eth_portfolio/structs/__init__.py
+++ eth_portfolio/structs/__init__.py
@@ -1,3 +1,28 @@
+"""
+This module provides the primary union type and specific ledger entry types used in the `eth_portfolio` package.
+
+The `__all__` list defines the public API for this module, which includes the main union type `LedgerEntry` and its specific types: `Transaction`, `InternalTransfer`, `TokenTransfer`, and `TransactionRLP`.
+
+Examples:
+    Importing the main union type and specific ledger entry types:
+    
+    >>> from eth_portfolio.structs import LedgerEntry, Transaction, InternalTransfer, TokenTransfer, TransactionRLP
+
+    Using the `LedgerEntry` union type to annotate a variable that can hold any ledger entry type:
+    
+    >>> entry: LedgerEntry = Transaction(...)
+    >>> entry = InternalTransfer(...)
+    >>> entry = TokenTransfer(...)
+    >>> entry = TransactionRLP(...)
+
+See Also:
+    - :class:`~eth_portfolio.structs.structs.LedgerEntry`
+    - :class:`~eth_portfolio.structs.structs.Transaction`
+    - :class:`~eth_portfolio.structs.structs.InternalTransfer`
+    - :class:`~eth_portfolio.structs.structs.TokenTransfer`
+    - :class:`~eth_portfolio.structs.structs.TransactionRLP`
+"""
+
 from eth_portfolio.structs.structs import (
     InternalTransfer,
     LedgerEntry,
@@ -5,7 +30,6 @@ from eth_portfolio.structs.structs import (
     Transaction,
     TransactionRLP,
 )
-
 
 __all__ = [
     # main union type
