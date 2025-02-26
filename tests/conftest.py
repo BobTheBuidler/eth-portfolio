88d390c067108e68d327ddb44015ea9ad27b2483
nvyncke <nvyncke@DESKTOP-FJSK0BM>
2025-02-26 15:28:00 -0500

    chore: auto-update documentation tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,7 @@
 """
 This module connects to the Brownie network specified by the `PYTEST_NETWORK`
-environment variable if it is not already connected. It also modifies the system
-path to include the current directory, allowing for importing modules from the
-project root.
+environment variable. It also modifies the system path to include the current
+directory, allowing for importing modules from the project root.
 
 Environment Variables:
     PYTEST_NETWORK: The name of the Brownie network to use for testing. This
