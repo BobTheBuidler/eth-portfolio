90625554773565e6a484deb48bd4322ad7ba9cc6
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-01 11:22:52 -0400

    chore: add _loaders/balances.py to build (#221)

--- eth_portfolio/_loaders/balances.py
+++ eth_portfolio/_loaders/balances.py
@@ -1,5 +1,6 @@
 import logging
 from decimal import InvalidOperation
+from typing import Final
 
 import y
 from y._decorators import stuck_coro_debugger
@@ -9,7 +10,10 @@ from eth_portfolio._decimal import Decimal
 from eth_portfolio._utils import _get_price
 from eth_portfolio.typing import Balance
 
-logger = logging.getLogger(__name__)
+
+_ZERO: Final = Decimal(0)
+
+logger: Final = logging.getLogger(__name__)
 
 
 @stuck_coro_debugger
@@ -40,16 +44,12 @@ async def load_token_balance(token: y.ERC20, address: Address, block: Block) -> 
         balance = await token.balance_of_readable(address, block, sync=False)
     except y.NonStandardERC20:
         logger.warning("NonStandardERC20 exc for %s", token)
-        balance = 0
+        balance = _ZERO
     if not balance:
         return Balance(token=token.address, block=block)
     price = await _get_price(token, block)
-    try:
-        rounded = round(Decimal(balance), 18)
-    except InvalidOperation as e:
-        rounded = Decimal(round(balance, 18))
     return Balance(
-        balance=rounded,
+        balance=round(balance, 18),
         usd_value=_calc_value(balance, price),
         token=token.address,
         block=block,
@@ -79,7 +79,15 @@ def _calc_value(balance, price) -> Decimal:
         return Decimal(0)
     # NOTE If balance * price returns a Decimal with precision < 18, rounding is both impossible and unnecessary.
     value = Decimal(balance) * Decimal(price)
+    return round(value, 18)
+
+
+_builtin_round: Final = round
+
+
+def round(value: Decimal, digits: int) -> Decimal:
+    # For a Decimal with precision < 18, rounding is both impossible and unnecessary.
     try:
-        return round(value, 18)
+        return _builtin_round(value, digits)  # type: ignore [return-value]
     except InvalidOperation:
         return value
