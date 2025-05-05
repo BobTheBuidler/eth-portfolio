43b55ca2bd07bd3cc7d5fb6075a6953609ed778f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-05-05 16:08:08 -0400

    feat: Portfolio repr (#239)
    * feat: Portfolio repr
    
    * chore: `black .`
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- eth_portfolio/portfolio.py
+++ eth_portfolio/portfolio.py
@@ -274,6 +274,14 @@ class Portfolio(a_sync.ASyncGenericBase):
             :class:`~web3.Web3`
         """
 
+    def __repr__(self) -> str:
+        # NOTE: should we show addresses? for now we won't
+        # string = f"{type(self).__name__}(addresses={list(map(str, self.addresses))}"
+        string = f"{type(self).__name__}(label={self.label}"
+        if self._start_block:
+            string += f", start_block={self._start_block}"
+        return string + f", load_prices={self.load_prices}, asynchronous={self.asynchronous})"
+
     def __getitem__(self, key: Address) -> PortfolioAddress:
         """
         Get a :class:`~eth_portfolio.address.PortfolioAddress` by its key.
@@ -367,7 +375,9 @@ class Portfolio(a_sync.ASyncGenericBase):
         """
         assert block
         return PortfolioBalances(
-            await a_sync.gather({address: address.describe(block, sync=False) for address in self}),
+            await a_sync.gather(
+                {address.address: address.describe(block, sync=False) for address in self}
+            ),
             block=block,
         )
 
