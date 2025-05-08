326c49f8c73139314c264db29edb55ff2be206eb
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-05-08 23:34:05 +0000

    fix: load dotenv

--- eth_portfolio_scripts/__init__.py
+++ eth_portfolio_scripts/__init__.py
@@ -5,6 +5,12 @@ environ["DANKMIDS_GANACHE_FORK"] = "0"
 environ["DANKMIDS_COLLECT_STATS"] = "0"
 
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
 from eth_portfolio_scripts._logging import logger, setup_logging
 
 
