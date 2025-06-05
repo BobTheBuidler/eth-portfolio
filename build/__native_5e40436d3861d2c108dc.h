64426437e535c29a8bb252772b325dd58d858604
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-06-05 16:06:17 -0400

    feat(cicd): build C files for source control (#310)
    * feat(cicd): build C files for source control
    
    * Create compile.yaml
    
    * chore: compile C files for source control
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Delete build/lib.linux-x86_64-cpython-312 directory
    
    * Delete build/temp.linux-x86_64-cpython-312/build directory
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ build/__native_5e40436d3861d2c108dc.h
@@ -0,0 +1,391 @@
+#ifndef MYPYC_NATIVE_5e40436d3861d2c108dc_H
+#define MYPYC_NATIVE_5e40436d3861d2c108dc_H
+#include <Python.h>
+#include <CPy.h>
+#ifndef MYPYC_DECLARED_tuple_T3OOO
+#define MYPYC_DECLARED_tuple_T3OOO
+typedef struct tuple_T3OOO {
+    PyObject *f0;
+    PyObject *f1;
+    PyObject *f2;
+} tuple_T3OOO;
+#endif
+
+#ifndef MYPYC_DECLARED_tuple_T2OO
+#define MYPYC_DECLARED_tuple_T2OO
+typedef struct tuple_T2OO {
+    PyObject *f0;
+    PyObject *f1;
+} tuple_T2OO;
+#endif
+
+#ifndef MYPYC_DECLARED_tuple_T3CIO
+#define MYPYC_DECLARED_tuple_T3CIO
+typedef struct tuple_T3CIO {
+    char f0;
+    CPyTagged f1;
+    PyObject *f2;
+} tuple_T3CIO;
+#endif
+
+#ifndef MYPYC_DECLARED_tuple_T4OOIO
+#define MYPYC_DECLARED_tuple_T4OOIO
+typedef struct tuple_T4OOIO {
+    PyObject *f0;
+    PyObject *f1;
+    CPyTagged f2;
+    PyObject *f3;
+} tuple_T4OOIO;
+#endif
+
+#ifndef MYPYC_DECLARED_tuple_T3IOO
+#define MYPYC_DECLARED_tuple_T3IOO
+typedef struct tuple_T3IOO {
+    CPyTagged f0;
+    PyObject *f1;
+    PyObject *f2;
+} tuple_T3IOO;
+#endif
+
+#ifndef MYPYC_DECLARED_tuple_T4CIOO
+#define MYPYC_DECLARED_tuple_T4CIOO
+typedef struct tuple_T4CIOO {
+    char f0;
+    CPyTagged f1;
+    PyObject *f2;
+    PyObject *f3;
+} tuple_T4CIOO;
+#endif
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *_lock;
+    double _ttl;
+} eth_portfolio____loaders____nonce___BlockCacheObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    vectorcallfunc vectorcall;
+} eth_portfolio____loaders____nonce_____mypyc_lambda__0_objObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_self__;
+    PyObject *_address;
+    PyObject *_block;
+    PyObject *_type;
+    PyObject *_value;
+    PyObject *_traceback;
+    PyObject *_arg;
+    CPyTagged ___mypyc_next_label__;
+    PyObject *___mypyc_temp__0;
+    tuple_T3OOO ___mypyc_temp__1;
+    PyObject *_nonce;
+    tuple_T3OOO ___mypyc_temp__2;
+    PyObject *_e;
+} eth_portfolio____loaders____nonce___get_nonce_at_block_envObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_env__;
+} eth_portfolio____loaders____nonce___get_nonce_at_block_genObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_self__;
+    PyObject *_address;
+    CPyTagged _nonce;
+    PyObject *_type;
+    PyObject *_value;
+    PyObject *_traceback;
+    PyObject *_arg;
+    CPyTagged ___mypyc_next_label__;
+    PyObject *_highest_known_nonce_lt_query;
+    PyObject *_lowest_known_nonce_gt_query;
+    PyObject *___mypyc_temp__3;
+    PyObject *___mypyc_temp__4;
+    char ___mypyc_temp__5;
+    PyObject *___mypyc_temp__6;
+    tuple_T3OOO ___mypyc_temp__7;
+    PyObject *___mypyc_temp__8;
+    CPyTagged ___mypyc_temp__9;
+    CPyTagged ___mypyc_temp__10;
+    PyObject *___mypyc_temp__11;
+    CPyTagged _n;
+    PyObject *_lo;
+    PyObject *_hi;
+    PyObject *___mypyc_temp__12;
+    tuple_T3OOO ___mypyc_temp__13;
+    CPyTagged _range_size;
+    PyObject *___mypyc_temp__14;
+    tuple_T3OOO ___mypyc_temp__15;
+    tuple_T3OOO ___mypyc_temp__16;
+    PyObject *___mypyc_temp__17;
+    tuple_T3OOO ___mypyc_temp__18;
+    PyObject *___mypyc_temp__19;
+    tuple_T3OOO ___mypyc_temp__20;
+    char _debug_logs_enabled;
+    PyObject *___mypyc_temp__21;
+    tuple_T3OOO ___mypyc_temp__22;
+    CPyTagged __nonce;
+    PyObject *_old_lo;
+    PyObject *___mypyc_temp__23;
+    tuple_T3OOO ___mypyc_temp__24;
+    CPyTagged _prev_block_nonce;
+} eth_portfolio____loaders____nonce___get_block_for_nonce_envObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_env__;
+} eth_portfolio____loaders____nonce___get_block_for_nonce_genObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_self__;
+    PyObject *_address;
+    CPyTagged _nonce;
+    PyObject *_lo;
+    PyObject *_hi;
+    CPyTagged _range_size;
+    PyObject *_type;
+    PyObject *_value;
+    PyObject *_traceback;
+    PyObject *_arg;
+    CPyTagged ___mypyc_next_label__;
+    CPyTagged _num_chunks;
+    CPyTagged _chunk_size;
+    PyObject *___mypyc_temp__25;
+    CPyTagged ___mypyc_temp__26;
+    CPyTagged ___mypyc_temp__27;
+    CPyTagged _i;
+    PyObject *_points;
+    PyObject *___mypyc_temp__28;
+    CPyTagged ___mypyc_temp__29;
+    PyObject *_point;
+    PyObject *___mypyc_temp__30;
+    tuple_T3OOO ___mypyc_temp__31;
+    PyObject *_nonces;
+    PyObject *___mypyc_temp__32;
+    CPyTagged ___mypyc_temp__33;
+    PyObject *___mypyc_temp__34;
+    CPyTagged ___mypyc_temp__35;
+    PyObject *_block;
+    CPyTagged _n;
+} eth_portfolio____loaders____nonce____get_area_envObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_env__;
+} eth_portfolio____loaders____nonce____get_area_genObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    uint32_t bitmap;
+    PyObject *___mypyc_self__;
+    PyObject *_type;
+    PyObject *_value;
+    PyObject *_traceback;
+    PyObject *_arg;
+    CPyTagged ___mypyc_next_label__;
+    PyObject *___mypyc_temp__36;
+    PyObject *___mypyc_temp__37;
+    char ___mypyc_temp__38;
+    PyObject *___mypyc_temp__39;
+    tuple_T3OOO ___mypyc_temp__40;
+    PyObject *___mypyc_temp__41;
+    double _ts;
+    PyObject *___mypyc_temp__42;
+    tuple_T3OOO ___mypyc_temp__43;
+    PyObject *_block;
+    tuple_T3OOO ___mypyc_temp__44;
+    PyObject *___mypyc_temp__45;
+    tuple_T3OOO ___mypyc_temp__46;
+    PyObject *___mypyc_temp__47;
+    tuple_T3OOO ___mypyc_temp__48;
+} eth_portfolio____loaders____nonce___get_block_number_envObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_env__;
+} eth_portfolio____loaders____nonce___get_block_number_genObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_self__;
+    PyObject *_token;
+    PyObject *_address;
+    PyObject *_block;
+    PyObject *_type;
+    PyObject *_value;
+    PyObject *_traceback;
+    PyObject *_arg;
+    CPyTagged ___mypyc_next_label__;
+    PyObject *___mypyc_temp__0;
+    tuple_T3OOO ___mypyc_temp__1;
+    PyObject *_balance;
+    tuple_T3OOO ___mypyc_temp__2;
+    PyObject *_token_address;
+    PyObject *___mypyc_temp__3;
+    tuple_T3OOO ___mypyc_temp__4;
+    PyObject *_price;
+} eth_portfolio____loaders___balances___load_token_balance_envObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_env__;
+} eth_portfolio____loaders___balances___load_token_balance_genObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_self__;
+    PyObject *_txhash;
+    PyObject *_type;
+    PyObject *_value;
+    PyObject *_traceback;
+    PyObject *_arg;
+    CPyTagged ___mypyc_next_label__;
+    PyObject *___mypyc_temp__0;
+    tuple_T3OOO ___mypyc_temp__1;
+} eth_portfolio____loaders___utils____get_transaction_receipt_envObject;
+
+typedef struct {
+    PyObject_HEAD
+    CPyVTableItem *vtable;
+    PyObject *___mypyc_env__;
+} eth_portfolio____loaders___utils____get_transaction_receipt_genObject;
+
+
+struct export_table_5e40436d3861d2c108dc {
+    PyObject **CPyStatic__nonce___logger;
+    PyObject **CPyStatic__nonce___logger_is_enabled;
+    PyObject **CPyStatic__nonce_____log;
+    CPyTagged *CPyStatic__nonce___DEBUG;
+    PyObject **CPyStatic__nonce___nonces;
+    PyObject **CPyStatic__nonce___locks;
+    PyObject **CPyStatic__nonce___get_transaction_count;
+    PyObject **CPyStatic__nonce___igather;
+    PyObject **CPyStatic__nonce___now;
+    PyObject **CPyStatic__nonce___BlockCache___lock;
+    PyTypeObject **CPyType__nonce___BlockCache;
+    PyObject *(*CPyDef__nonce___BlockCache)(void);
+    PyTypeObject **CPyType__nonce_____mypyc_lambda__0_obj;
+    PyObject *(*CPyDef__nonce_____mypyc_lambda__0_obj)(void);
+    PyTypeObject **CPyType__nonce___get_nonce_at_block_env;
+    PyObject *(*CPyDef__nonce___get_nonce_at_block_env)(void);
+    PyTypeObject **CPyType__nonce___get_nonce_at_block_gen;
+    PyObject *(*CPyDef__nonce___get_nonce_at_block_gen)(void);
+    PyTypeObject **CPyType__nonce___get_block_for_nonce_env;
+    PyObject *(*CPyDef__nonce___get_block_for_nonce_env)(void);
+    PyTypeObject **CPyType__nonce___get_block_for_nonce_gen;
+    PyObject *(*CPyDef__nonce___get_block_for_nonce_gen)(void);
+    PyTypeObject **CPyType__nonce____get_area_env;
+    PyObject *(*CPyDef__nonce____get_area_env)(void);
+    PyTypeObject **CPyType__nonce____get_area_gen;
+    PyObject *(*CPyDef__nonce____get_area_gen)(void);
+    PyTypeObject **CPyType__nonce___get_block_number_env;
+    PyObject *(*CPyDef__nonce___get_block_number_env)(void);
+    PyTypeObject **CPyType__nonce___get_block_number_gen;
+    PyObject *(*CPyDef__nonce___get_block_number_gen)(void);
+    PyObject *(*CPyDef__nonce_____mypyc_lambda__0_obj_____get__)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_instance, PyObject *cpy_r_owner);
+    PyObject *(*CPyDef__nonce_____mypyc_lambda__0_obj_____call__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_nonce_at_block_gen_____mypyc_generator_helper__)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef__nonce___get_nonce_at_block_gen_____next__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_nonce_at_block_gen___send)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef__nonce___get_nonce_at_block_gen_____iter__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_nonce_at_block_gen___throw)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback);
+    PyObject *(*CPyDef__nonce___get_nonce_at_block_gen___close)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_nonce_at_block_gen_____await__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_nonce_at_block)(PyObject *cpy_r_address, PyObject *cpy_r_block);
+    PyObject *(*CPyDef__nonce___get_block_for_nonce_gen_____mypyc_generator_helper__)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef__nonce___get_block_for_nonce_gen_____next__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_block_for_nonce_gen___send)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef__nonce___get_block_for_nonce_gen_____iter__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_block_for_nonce_gen___throw)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback);
+    PyObject *(*CPyDef__nonce___get_block_for_nonce_gen___close)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_block_for_nonce_gen_____await__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_block_for_nonce)(PyObject *cpy_r_address, CPyTagged cpy_r_nonce);
+    PyObject *(*CPyDef__nonce____get_area_gen_____mypyc_generator_helper__)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef__nonce____get_area_gen_____next__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce____get_area_gen___send)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef__nonce____get_area_gen_____iter__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce____get_area_gen___throw)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback);
+    PyObject *(*CPyDef__nonce____get_area_gen___close)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce____get_area_gen_____await__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce____get_area)(PyObject *cpy_r_address, CPyTagged cpy_r_nonce, PyObject *cpy_r_lo, PyObject *cpy_r_hi, CPyTagged cpy_r_range_size);
+    PyObject *(*CPyDef__nonce____update_nonces)(PyObject *cpy_r_address, CPyTagged cpy_r_nonce, PyObject *cpy_r_block);
+    CPyTagged (*CPyDef__nonce____get_num_chunks)(CPyTagged cpy_r_range_size);
+    char (*CPyDef__nonce___BlockCache_____mypyc_defaults_setup)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_block_number_gen_____mypyc_generator_helper__)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef__nonce___get_block_number_gen_____next__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_block_number_gen___send)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef__nonce___get_block_number_gen_____iter__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_block_number_gen___throw)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback);
+    PyObject *(*CPyDef__nonce___get_block_number_gen___close)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_block_number_gen_____await__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef__nonce___get_block_number)(void);
+    char (*CPyDef__nonce_____top_level__)(void);
+    PyObject **CPyStatic_balances____ZERO;
+    PyObject **CPyStatic_balances___logger;
+    PyObject **CPyStatic_balances____builtin_round;
+    PyTypeObject **CPyType_balances___load_token_balance_env;
+    PyObject *(*CPyDef_balances___load_token_balance_env)(void);
+    PyTypeObject **CPyType_balances___load_token_balance_gen;
+    PyObject *(*CPyDef_balances___load_token_balance_gen)(void);
+    PyObject *(*CPyDef_balances___load_token_balance_gen_____mypyc_generator_helper__)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef_balances___load_token_balance_gen_____next__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef_balances___load_token_balance_gen___send)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef_balances___load_token_balance_gen_____iter__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef_balances___load_token_balance_gen___throw)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback);
+    PyObject *(*CPyDef_balances___load_token_balance_gen___close)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef_balances___load_token_balance_gen_____await__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef_balances___load_token_balance)(PyObject *cpy_r_token, PyObject *cpy_r_address, PyObject *cpy_r_block);
+    PyObject *(*CPyDef_balances____calc_value)(PyObject *cpy_r_balance, PyObject *cpy_r_price);
+    PyObject *(*CPyDef_balances___round)(PyObject *cpy_r_value, CPyTagged cpy_r_digits);
+    char (*CPyDef_balances_____top_level__)(void);
+    PyObject **CPyStatic_utils___Raw;
+    PyObject **CPyStatic_utils___get_transaction_receipt;
+    PyObject **CPyStatic_utils_____get_transaction_receipt;
+    PyTypeObject **CPyType_utils____get_transaction_receipt_env;
+    PyObject *(*CPyDef_utils____get_transaction_receipt_env)(void);
+    PyTypeObject **CPyType_utils____get_transaction_receipt_gen;
+    PyObject *(*CPyDef_utils____get_transaction_receipt_gen)(void);
+    PyObject *(*CPyDef_utils____get_transaction_receipt_gen_____mypyc_generator_helper__)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef_utils____get_transaction_receipt_gen_____next__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef_utils____get_transaction_receipt_gen___send)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_arg);
+    PyObject *(*CPyDef_utils____get_transaction_receipt_gen_____iter__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef_utils____get_transaction_receipt_gen___throw)(PyObject *cpy_r___mypyc_self__, PyObject *cpy_r_type, PyObject *cpy_r_value, PyObject *cpy_r_traceback);
+    PyObject *(*CPyDef_utils____get_transaction_receipt_gen___close)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef_utils____get_transaction_receipt_gen_____await__)(PyObject *cpy_r___mypyc_self__);
+    PyObject *(*CPyDef_utils____get_transaction_receipt)(PyObject *cpy_r_txhash);
+    char (*CPyDef_utils_____top_level__)(void);
+    PyObject *(*CPyDef__argspec___get_args_type)(PyObject *cpy_r_sample);
+    PyObject *(*CPyDef__argspec___get_kwargs_type)(PyObject *cpy_r_sample);
+    PyObject *(*CPyDef__argspec___get_return_type)(PyObject *cpy_r_sample);
+    tuple_T3OOO (*CPyDef__argspec___get_types)(PyObject *cpy_r_sample);
+    char (*CPyDef__argspec_____top_level__)(void);
+    CPyTagged *CPyStatic__config___REORG_BUFFER;
+    char (*CPyDef__config_____top_level__)(void);
+    PyObject **CPyStatic__shitcoins___shitcoins;
+    char (*CPyDef__shitcoins_____top_level__)(void);
+    CPyTagged *CPyStatic_constants___NUM_WALLETS;
+    PyObject **CPyStatic_constants___ADDRESSES;
+    PyObject **CPyStatic_constants___TRANSFER_SIGS;
+    PyObject **CPyStatic_constants___ETH_LIKE;
+    PyObject **CPyStatic_constants___BTC_LIKE;
+    PyObject **CPyStatic_constants___INTL_STABLECOINS;
+    char (*CPyDef_constants_____top_level__)(void);
+};
+#endif
