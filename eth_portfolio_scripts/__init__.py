a7ce78f68c5fedaa7ac73c52276fecb5dd005769
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2025-11-12 08:40:48 +0000

    feat: add postgres container to exporter docker deployment

--- eth_portfolio_scripts/__init__.py
+++ eth_portfolio_scripts/__init__.py
@@ -1,8 +1,29 @@
 from os import environ
 
-
+# Set DANKMIDS envs
 environ["DANKMIDS_GANACHE_FORK"] = "0"
 environ["DANKMIDS_COLLECT_STATS"] = "0"
+
+YDB_CONNECTION_PARAMS = {
+    "DB_PROVIDER": "postgres",
+    "DB_HOST": "127.0.0.1",
+    "DB_PORT": "5445",
+    "DB_USER": "eth-portfolio-exporter",
+    "DB_PASSWORD": "eth-portfolio-exporter",
+    "DB_DATABASE": "eth-portfolio-exporter",
+}
+
+for key in YDB_CONNECTION_PARAMS:
+    existing = environ.get(key)
+    if existing is not None:
+        raise RuntimeError(
+            f"Environment variable {key} is already set to '{existing}', "
+            f"but eth-portfolio requires it to be unset."
+        )
+
+# Set the db connection params for ypricemagic so it
+# uses our Postgres container instead of sqlite
+environ.update(YDB_CONNECTION_PARAMS)
 
 
 from dotenv import load_dotenv
