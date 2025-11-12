20babd10294151039ab22fbf16c05e85e53939f4
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-11-12 15:17:33 -0400

    fix: reduce default exporter concurrency 40 -> 30

--- eth_portfolio_scripts/_portfolio.py
+++ eth_portfolio_scripts/_portfolio.py
@@ -58,7 +58,7 @@ class ExportablePortfolio(Portfolio):
         *,
         start_block: int = 0,
         label: str = _DEFAULT_LABEL,
-        concurrency: int = 40,
+        concurrency: int = 30,
         load_prices: bool = True,
         get_bucket: Callable[[ChecksumAddress], Awaitable[str]] = None,
         num_workers_transactions: int = 1000,
