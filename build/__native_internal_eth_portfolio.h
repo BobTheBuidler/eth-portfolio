cf0e8e1b372d0c59fe293fa943ff6dda0c8cf7a7
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-11-29 02:02:53 -0500

    chore: add shitcoin to shitcoins (#516)
    * chore: add shitcoin to shitcoins
    
    * chore: compile C files for source control
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- build/__native_internal_eth_portfolio.h
+++ build/__native_internal_eth_portfolio.h
@@ -6,7 +6,7 @@
 
 int CPyGlobalsInit(void);
 
-extern PyObject *CPyStatics[712];
+extern PyObject *CPyStatics[713];
 extern const char * const CPyLit_Str[];
 extern const char * const CPyLit_Bytes[];
 extern const char * const CPyLit_Int[];
